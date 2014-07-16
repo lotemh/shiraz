607936f34cd6cf256764b53c65583e8c24ae8e6d
lotemh <lotemh@wix.com>
2014-07-16 17:34:50 +0300

    adding server side and example client

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var placesToEat = ['גבאי', 'סירים']; //and so on...
+var lastDayOfUpdate = new Date();
+var foodOfTheDayIndex = 0;
+
+var getFoodOfTheDay = function() {
+    var dateOfToday = new Date();
+    if (dateOfToday.getMonth() === lastDayOfUpdate.getMonth()
+        &&
+        dateOfToday.getDate() !== lastDayOfUpdate.getDate()) {
+        updateFoodOfTheDayIndex();
+    }
+    return placesToEat[foodOfTheDayIndex];
+};
+
+var updateFoodOfTheDayIndex = function(){
+    foodOfTheDayIndex++;
+    if (foodOfTheDayIndex >= placesToEat.length)
+        foodOfTheDayIndex = 0;
+};
+
+app.use('/', express.static(__dirname));
+
+app.get('/getFood', function(req, res){
+    res.send(getFoodOfTheDay());
+});
+
+var server = app.listen(3000);
